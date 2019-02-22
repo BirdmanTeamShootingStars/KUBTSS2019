724381fe785931638f128f416db382ab64d36362
KentaFujiki <fujikikenta0910@gmail.com>
2019-02-22 15:30:14 +0900

    a

--- app/src/main/java/com/example/hongu/apaapa/Alert.java
+++ app/src/main/java/com/example/hongu/apaapa/Alert.java
@@ -1,8 +1,10 @@
 package com.example.hongu.apaapa;
 
-import
+import android.location.LocationListener;
+import android.location.Location;
 
 public class Alert {
     private double[][] vertex = {{135/*latitude*/, 35/*longitude*/}, {136,36}/*, {}, {}, {},…*/};
+        private double a = Location.distanceBetween();
 
 }
