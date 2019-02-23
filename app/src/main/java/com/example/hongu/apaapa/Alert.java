2d46b5027c0b6c3f13e723e198cb1daaf7b451e2
KentaFujiki <fujikikenta0910@gmail.com>
2019-02-23 15:45:59 +0900

    a

--- app/src/main/java/com/example/hongu/apaapa/Alert.java
+++ app/src/main/java/com/example/hongu/apaapa/Alert.java
@@ -1,10 +1,38 @@
 package com.example.hongu.apaapa;
 
-import android.location.LocationListener;
-import android.location.Location;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
-public class Alert {
-    private double[][] vertex = {{135/*latitude*/, 35/*longitude*/}, {136,36}/*, {}, {}, {},…*/};
-        private double a = Location.distanceBetween();
+public class Alert extends AppCompatActivity {
+    SoundPool soundPool;
+    private int sound;
 
+    public void Alert {
+        soundPool = null;
+        AudioAttributes attr = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        SoundPool pool = new SoundPool.Builder()
+                .setAudioAttributes(attr)
+                .setMaxStreams(1)
+                .build();
+
+        sound = soundPool.load(this, R.sound.alert, 1);
+
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                if (0 == status) {
+                    soundPool.play(sampleId,1.0F, 1.0F, 0, -1, 1.0F);
+                }
+            }
+        });
+    }
 }
